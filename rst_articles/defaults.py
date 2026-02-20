d872280dc487e1925e3942d139f66a6b25650bd4
aus <aus@email.es>
2026-02-20 23:27:14 +0100

    Added ability to plot using matplotlib

--- rst_articles/defaults.py
+++ rst_articles/defaults.py
@@ -5,6 +5,13 @@ external_extensions = {
 	'sphinxcontrib.bibtex',
 }
 
+try:
+	import matplotlib  # noqa: F401
+
+	external_extensions.add('matplotlib.sphinxext.plot_directive')
+except ImportError:
+	pass
+
 custom_extensions = set()
 for extension in Path('rst_articles/_ext').glob('*.py'):
 	name = extension.name
